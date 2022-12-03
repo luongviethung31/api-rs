ed152ac8b0aac406a5d6c01450325fdf4483713c
Luong Mau Viet Hung <luongmauviethung31.01@gmail.com>
2022-12-03 22:54:32 +0700

    fix api rs

--- wsgi.py
+++ wsgi.py
@@ -28,7 +28,7 @@ def create_task():
         abort(400)
     bookIds = request.json["bookIds"]
     userId = request.json["userId"]
-    userIds = np.full(len(bookIds), userId, dtype="int32");
+    userIds = np.full(len(bookIds), userId, dtype="int32")
     results = reconstructed_model.predict([tf.convert_to_tensor(userIds, dtype=tf.int32), np.array(tf.convert_to_tensor(bookIds, dtype=tf.int32))],
                                 batch_size=100, verbose=0)
     map_item_score = {}
